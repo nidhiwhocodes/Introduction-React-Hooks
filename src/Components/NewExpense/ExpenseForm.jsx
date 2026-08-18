31780093c59b62e9ad69bce51c1598e3bb620a83
Nidhi ponnalwar <nidhiponnalwar13@gmail.com>
2026-08-18 22:07:44 +0530

    Add form submit event

--- src/Components/NewExpense/ExpenseForm.jsx
+++ src/Components/NewExpense/ExpenseForm.jsx
@@ -15,16 +15,20 @@ const ExpenseForm = () => {
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };
-
-   const expenseData = {
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+     const expenseData = {
       title: enteredTitle,
       amount: enteredAmount,
-      date: enteredDate,
+      date: new Date(enteredDate),
     };
     console.log(expenseData);
+  };
+
+  
 
   return (
-    <form>
+    <form onSubmit={formSubmitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
