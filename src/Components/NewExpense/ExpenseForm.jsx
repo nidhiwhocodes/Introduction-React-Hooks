36ba38e9241131571825f7b48c8bb04a5b1513cc
Nidhi ponnalwar <nidhiponnalwar13@gmail.com>
2026-08-20 16:56:33 +0530

    Pass the expense data from the ExpenseForm component to the NewExpense component, and then pass it up to the App component using a callback function. Update the state in the App component to include the new expense.

--- src/Components/NewExpense/ExpenseForm.jsx
+++ src/Components/NewExpense/ExpenseForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -19,10 +19,10 @@ const ExpenseForm = () => {
     event.preventDefault();
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      price: enteredAmount,
       date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
