a1eefeca917858bbe739504bf84e570bef81e5ce
Nidhi ponnalwar <nidhiponnalwar13@gmail.com>
2026-08-20 17:17:18 +0530

    updated

--- src/Components/Expenses/ExpenseItem.jsx
+++ src/Components/Expenses/ExpenseItem.jsx
@@ -1,24 +1,18 @@
-// Write your code at the relevant places in the code below
-import  { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle] = useState(props.title);
-  function buttonClickHandler() {
-    setTitle("New Title");
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
-      <button onClick={buttonClickHandler}> Change Title</button>
+      
     </Card>
   );
 };
